7a06a50ec5c5c86a0399f84e64ef406273c94305
billNKB <155600630+billNKB@users.noreply.github.com>
2024-06-17 06:57:34 +0100

    Bill completed Task 1 (model-creation)

--- bill/models.py
+++ bill/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+STATUS_CHOICE = (
+    ("completed", "Completed"),
+    ("not_completed", "Not Completed"),
+)
+
+'''
+class User(models.Model):
+    name = models.CharField(max_length=25)
+    password = models.CharField(max_length=20)
+    email = models.EmailField(max_length=70, unique=True)
+'''
+class BillTask(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=16, choices=STATUS_CHOICE, default="not_completed")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class BillTag(models.Model):
+    name = models.CharField(max_length=30)
+    task = models.ManyToManyField(BillTask)
+
